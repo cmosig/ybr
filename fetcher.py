5b73b741daeffb63122a251acefab40a026debe0
numberking123 <clemens.mosig@gmail.com>
2019-07-09 15:08:04 +0200

    fetching video titles for a series in module fetcher

---
+++ fetcher.py
@@ -0,0 +1,25 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+
+def fetch_latest_videotitles(channel_name,series_regex=""):
+    """returns latest videos from channel optionally filtering for series"""
+    base_url = "https://www.youtube.com/user/<channel>/videos"
+    url = base_url.replace("<channel>",channel_name)
+    channel_bs = BeautifulSoup(urllib.request.urlopen(url),"html.parser")
+    video_elements = channel_bs.findAll("a", {"class":"yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2"})
+    video_titles = pd.Series(list(map(get_title,video_elements)))
+    titles = video_titles[video_titles.apply(match_regex,args=(series_regex,))].tolist()
+    return titles
+
+def get_title(element):
+    return element.get("title")
+
+def match_regex(title,regex):
+    return bool(re.match(regex,title))
+
+#print(fetch_latest_videotitles("EthosLab",r"Minecraft - Diversity 3 #[0..9]*"))
+#print(fetch_latest_videotitles("EthosLab",r"Etho Plays Minecraft - Episode [0..9]*"))
+
