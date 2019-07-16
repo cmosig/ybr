a06d4ac5a9e0b48ff8a1260f9774d44348cf32d4
numberking123 <clemens.mosig@gmail.com>
2019-07-16 13:00:20 +0200

    stuff

--- fetcher.py
+++ fetcher.py
@@ -6,14 +6,14 @@ import storage as st
 
 
 def fetch_latest_videotitles(channel_name="",series_regex=""):
-    base_url = "https://www.youtube.com/user/<channel>/videos"
+    url_videos = "https://www.youtube.com/user/<channel>/videos"
+    url_yt = "https://www.youtube.com"
     
     if (channel_name == ""):
         #check entire series list
         to_fetch = st.get_series_asdict()
     elif (series_regex == ""):
         #check only of specific channel
-        #TODO
         print("TODO")
     else:
         #check channel - series pair
@@ -22,33 +22,39 @@ def fetch_latest_videotitles(channel_name="",series_regex=""):
     for s_name,c_name in to_fetch.items():
 
         #fetch latest episodes
-        url = base_url.replace("<channel>",c_name)
+        url = url_videos.replace("<channel>",c_name)
         channel_bs = BeautifulSoup(urllib.request.urlopen(url),"html.parser")
         video_elements = channel_bs.findAll("a", {"class":"yt-uix-sessionlink yt-uix-tile-link spf-link yt-ui-ellipsis yt-ui-ellipsis-2"})
-        video_titles = pd.Series(list(map(get_title,video_elements)))
-        titles = video_titles[video_titles.apply(match_regex,args=(s_name,))].tolist()
+        video_titles = list(map(get_title,video_elements))
+        video_urls = list(map(get_url,video_elements))
+        videos = pd.DataFrame.from_dict({"title":video_titles,"url":video_urls})
+        videos = videos[videos["title"].apply(match_regex,args=(s_name,))]
+        videos = [tuple(x) for x in videos.values]
+        #titles = video_titles[video_titles.apply(match_regex,args=(s_name,))].tolist()
 
         #find the latest episode for a given series-channel pair check if there are new episodes    
         current_latest = st.get_latest_episode_name(s_name,c_name)
 
-        if (len(titles) == 0):
+        if (len(videos) == 0):
             continue
-        if(current_latest == "" or titles[0] != current_latest):
-            st.set_latest(titles[0],s_name,c_name)
-        for i in range(len(titles)):
-            #TODO also add URL
-            #going through all found titles matching the given series-channel pair
+        if(current_latest == "" or videos[0][0] != current_latest):
+            st.set_latest(videos[0][0],s_name,c_name)
+        for i in range(len(videos)):
+            #going through all found videos matching the given series-channel pair
             if(current_latest == ""):
-                st.add_new_episode(titles[0])
+                st.add_new_episode(videos[0][0],url_yt + videos[0][1])
                 break
-            if(titles[i] == current_latest):
+            if(videos[i][0] == current_latest):
                 break
             else:
-                st.add_new_episode(titles[i])
+                st.add_new_episode(videos[i][0],url_yt + videos[i][1])
     
 
 def get_title(element):
     return element.get("title")
+
+def get_url(element):
+    return element.get("href")
 
 def match_regex(title,regex):
     return bool(re.match(regex,title))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='ybr',
-    version='0.2',
+    version='0.3',
     entry_points={
         'console_scripts': [
             'ybr=ybr:main'
