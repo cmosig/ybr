eeebce652a3b06de45805a8ab571f59bee1ed7cb
numberking123 <clemens.mosig@gmail.com>
2019-07-17 11:14:08 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 setup(
     name='ybr',
     version='0.3',
+    install_requires=[
+        'bs4',
+        'pandas',
+        'tabulate'
+        ],
     entry_points={
         'console_scripts': [
             'ybr=ybr:main'
-        ]
+        ],
     }
 )
